f94ed9656657a6af28068701ffb3b54d2be17725
AshF000 <ashfiq_islam@yahoo.com>
2022-10-26 16:12:47 +0600

    Submit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 $(function () {
-
   //ACTIVE NAVIGATION (MENU)
   $(".nav_menu_opt").click(function () {
-    $('.nav_menu_opt').removeClass('active');
-    $(this).addClass('active');
+    $(".nav_menu_opt").removeClass("nav_menu_active");
+    $(this).addClass("nav_menu_active");
   });
 
+  //FOOTER MENU
+  $(".foot_menu_opt").click(function () {
+    $(".foot_menu_opt").removeClass("foot_menu_active");
+    $(this).addClass("foot_menu_active");
+  });
 
   //STICKY NAVIGATION (MENU)
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function () {
     var scroll = $(window).scrollTop();
     if (scroll < 245) {
       $(".navbar").removeClass("sticky");
@@ -17,28 +21,29 @@ $(function () {
     }
   });
 
-
   // UPDATE SLIDER
-  $('.update_right').slick({
+  $(".update_right").slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
-    prevArrow: '.update_left_arrow',
-    nextArrow: '.update_right_arrow',
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }, ]
+    prevArrow: ".update_left_arrow",
+    nextArrow: ".update_right_arrow",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   /////////////////////////////////////////////////////////////////////////////////////////////
 
   // TIMER FOR THE DEAL
   const countdown = () => {
-    const countDate = new Date("January 01, 2023 00:00:00").getTime()
+    const countDate = new Date("January 01, 2023 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
@@ -61,33 +66,32 @@ $(function () {
     document.querySelector(".minutes").innerText = textMinute;
     document.querySelector(".seconds").innerText = textSecond;
   };
-  setInterval(countdown, 1000)
+  setInterval(countdown, 1000);
 
   /////////////////////////////////////////////////////////////////////////////////////////////
 
-  //ACTIVE FOOTER MENU
-  $(".foot_menu_opt").click(function () {
-    $('.foot_menu_opt').removeClass('foot_menu_active');
-    $(this).addClass('foot_menu_active');
-  });
-
+  // //ACTIVE FOOTER MENU
+  // $(".foot_menu_opt").click(function () {
+  //   $(".foot_menu_opt").removeClass("foot_menu_active");
+  //   $(this).addClass("foot_menu_active");
+  // });
 
   // //SCROLL TO TOP BUTTON
-  $(window).on('scroll', function (event) {
+  $(window).on("scroll", function (event) {
     if ($(this).scrollTop() > 600) {
-      $('.taptotop').fadeIn(200)
+      $(".taptotop").fadeIn(200);
     } else {
-      $('.taptotop').fadeOut(200)
+      $(".taptotop").fadeOut(200);
     }
   });
 
-  $('.taptotop').on('click', function (event) {
+  $(".taptotop").on("click", function (event) {
     event.preventDefault();
-    $('html, body').animate({
-      scrollTop: 0,
-    }, 300);
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
   });
-
-
-
 });
